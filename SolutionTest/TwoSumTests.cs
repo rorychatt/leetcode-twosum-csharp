62103eb798eaf3b8b1fb0f15ebe651d0398b0a76
rorychatt <pavel.rinne@gmail.com>
2024-07-15 09:40:12 +0200

    Info: better test input

--- SolutionTest/TwoSumTests.cs
+++ SolutionTest/TwoSumTests.cs
@@ -6,7 +6,7 @@ namespace SolutionTest;
 public class TwoSumTests
 {
     [Theory]
-    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 17, new int[] { 0, 3 })]
     [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
     [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
     public void Test1(int[] input, int target, int[] expected)
